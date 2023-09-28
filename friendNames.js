0c212f4d31ff68a450c7b747ee9ea0ebf8b26c7f
Junaid Ul Haque Sheikh <145222963+junaidsheikh02@users.noreply.github.com>
2023-09-28 08:42:37 -0700

    EXERCISE:>>>0011
    CONVERT PYTHON CODE INTO TYPESCRIPT CODE:>>>
    // Store the names of your friends in an array
    const names: string[] = ["Alice", "Bob", "Charlie", "David"];
    
    // Print each person's name one at a time
    for (const name of names) {
        console.log(name);
    }
    
    
    
    
    
    
    CONVERT PYTHON CODE INTO JAVASCRIPT CODE:>>>
    "use strict";
    // Store the names of your friends in an array
    const names = ["Alice", "Bob", "Charlie", "David"];
    // Print each person's name one at a time
    for (const name of names) {
        console.log(name);
    }

---
+++ friendNames.js
@@ -0,0 +1,7 @@
+"use strict";
+// Store the names of your friends in an array
+const names = ["Alice", "Bob", "Charlie", "David"];
+// Print each person's name one at a time
+for (const name of names) {
+    console.log(name);
+}
---
+++ friendNames.ts
@@ -0,0 +1,7 @@
+// Store the names of your friends in an array
+const names: string[] = ["Alice", "Bob", "Charlie", "David"];
+
+// Print each person's name one at a time
+for (const name of names) {
+    console.log(name);
+}
